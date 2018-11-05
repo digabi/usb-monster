6017ae04d206e183b44906c33b0658dbcea92fcb
Matti Lattu <matti.lattu@ylioppilastutkinto.fi>
2018-11-05 19:01:53 +0200

    Renamed dd-hubs package and added dd-curses package

---
+++ dd-curses/create-deb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p deb-root/usr/local/lib/digabi-dd-curses
+
+cp $(git ls-files | grep -v create-deb.sh) deb-root/usr/local/lib/digabi-dd-curses/
+
+fpm -C deb-root/ -s dir --name digabi-dd-curses --architecture native -t deb --version "1.0.${BUILD_NUMBER}" --depends pv --depends python --depends python-psutil .
--- dd-hubs/create-deb.sh
+++ dd-hubs/create-deb.sh
@@ -4,6 +4,6 @@ set -e
 
 mkdir -p deb-root/usr/local/lib/digabi-dd-curses
 
-cp $(git ls-files | grep -v create-deb.sh) deb-root/usr/local/lib/digabi-dd-curses/
+cp $(git ls-files | grep -v create-deb.sh) deb-root/usr/local/lib/digabi-dd-hubs/
 
-fpm -C deb-root/ -s dir --name digabi-dd-curses --architecture native -t deb --version "1.0.${BUILD_NUMBER}" --depends pv --depends python .
+fpm -C deb-root/ -s dir --name digabi-dd-hubs --architecture native -t deb --version "1.0.${BUILD_NUMBER}" --depends pv --depends python .
